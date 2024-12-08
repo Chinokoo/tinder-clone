150c8bb74b2edf3eb5ef8da6b65e9ab948c6a86e
Peter <peterchinokoo@gmail.com>
2024-12-08 19:06:26 +0200

    configure server

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,9 @@
 import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config({ path: "./.env" });
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
-app.listen(3000, () => console.log("Server is running on port 3000"));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
